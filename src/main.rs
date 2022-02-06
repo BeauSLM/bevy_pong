73cd7b018f9580f223dcec2f825b1841d569cae2
Beau McCartney <mccartney.beausl@gmail.com>
2022-02-06 10:11:57 -0700

    Add some basics

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,41 @@
 use bevy::prelude::*;
 
+const FOREGROUND: Color = Color::rgb(0.7, 0.7, 0.7);
+const BACKGROUND: Color = Color::rgb(0.04, 0.04, 0.04);
+const PADDLE_SPEED: f32 = 1.;
+const BALL_SPEED: f32 = 1.;
+
+#[derive(Component)]
+struct LeftPaddle;
+
+#[derive(Component)]
+struct RightPaddle;
+
+#[derive(Component)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
 fn main() {
     App::new()
-        .add_system(hello_world_system)
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(BACKGROUND))
+        .add_startup_system(setup)
         .run();
 }
 
-fn hello_world_system() {
-    println!("Hello, world!");
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: FOREGROUND,
+            custom_size: Some(Vec2::new(2., 30.)),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .insert(LeftPaddle);
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.orthographic_projection.scale = 1. / 10.;
+    commands.spawn_bundle(camera);
 }
